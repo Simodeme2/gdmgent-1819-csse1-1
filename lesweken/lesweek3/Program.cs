c71f3ba78eb5e3552231fa9d2e61a86cf3b046dd
krisra <kristof.raes@arteveldehs.be>
2018-10-10 12:08:04 +0200

    WIP

---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Het afdrukken van een tekst in de console.
+            Console.WriteLine("Dag!");
+
+            /*
+                Dus deze applicatie doet momenteel nog niet veel...
+             */
+
+            // Rekenmachine
+            //int c, d;
+            int a = 5;
+            int b = 6;
+            int resultaat1 = optellen(a, b);
+            int resultaat2 = deling(a, b);
+            int resultaat3 = verhogen(a);
+        }
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int verhogen(int a)
+        {
+            return a++;
+        }
+
+        static int deling(int a, int b){
+            return a/b;
+        }
+    }
+}
